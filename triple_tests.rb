34ef53e2524850e751d2c5b057cd512f83304ddc
Kyle Edgette <edgettek@gmail.com>
2017-09-12 22:49:04 -0400

    Implemented up to min()

--- triple_tests.rb
+++ triple_tests.rb
@@ -239,37 +239,503 @@ class TripleTests < Minitest::Test
 
     assert_nil result, nil
   end
-  
+
 
 
   # each_with_index(*args) { |obj, i| block } → enum
+
+  # def test_each_with_ints
+  #
+  #   result = @integers.each_with_index do |num, index|
+  #     print num + 'at index ' + index
+  #   end
+  #
+  #   assert_equal result, @integers
+  #
+  # end
+
+
   # each_with_object(obj) { |(*args), memo_obj| ... } → obj
+
+  def test_each_with_object_ints
+
+    result = @integers.each_with_object([]) do |i, mem|
+      mem << i * 3
+    end
+
+    assert_equal result, [3, 6, 9]
+  end
+
+  def test_each_with_object_string
+
+    result = @strings.each_with_object([]) do |i, mem|
+      mem << i + '!'
+    end
+
+
+  assert_equal result, ["Kyle!", "Preston!", "Renato!"]
+  end
+
+
   # entries(*args) → array
+
+  def test_entries_ints
+
+    result = @integers.entries
+
+    assert_equal result, [1, 2, 3]
+
+  end
+
+  def test_entries_string
+
+    result = @strings.entries
+
+    assert_equal result, ["Kyle", "Preston", "Renato"]
+
+  end
+
   # find(ifnone = nil) { |obj| block } → obj or nil
+
+  def test_find_1_ints
+    result = @integers.find do |number|
+      number%2 == 0
+    end
+
+    assert_equal result, 2
+  end
+
+  def test_find_2_ints
+    result = @integers.find(nil) do |number|
+      number%5 == 0
+    end
+
+    assert_nil result, nil
+  end
+
+  def test_find_1_strings
+    result = @strings.find(nil) do |name|
+      name > 'Allison'
+    end
+
+    assert_equal result, 'Kyle'
+  end
+
+  def test_find_2_strings
+    result = @strings.find(nil) do |name|
+      name < 'Allison'
+    end
+
+    assert_nil result, nil
+  end
+
+
   # find_all { |obj| block } → array % same as select
+
+  def test_find_all_ints
+
+    result = @integers.find_all do |num|
+      num % 2 == 0
+    end
+
+    assert_equal result, [2]
+
+  end
+
+  def test_find_all_strings
+
+    result = @strings.find_all do |name|
+      name.eql? 'Kyle'
+    end
+
+    assert_equal result, ['Kyle']
+
+  end
+
   # find_index(value) → int or nil
+
+  def test_find_index_ints
+
+    result = @integers.find_index(3)
+
+    assert_equal result, 2
+
+  end
+
+  def test_find_index_ints_2
+
+    result = @integers.find_index(45)
+
+    assert_nil result, nil
+
+  end
+
+  def test_find_index_strings
+
+    result = @strings.find_index("Preston")
+
+    assert_equal result, 1
+
+  end
+
+  def test_find_index_strings_2
+
+    result = @strings.find_index("Anna")
+
+    assert_nil result, nil
+
+  end
+
   # find_index { |obj| block } → int or nil
+
+  def test_find_index_ints_block
+
+    result = @integers.find_index do |num|
+      num > 1
+    end
+
+    assert_equal result, 1
+
+  end
+
+  def test_find_index_ints_2_block
+
+    result = @integers.find_index do |num|
+      num > 30
+    end
+
+    assert_nil result, nil
+
+  end
+
+  def test_find_index_strings_block
+
+    result = @strings.find_index do |name|
+      name < 'Quinn'
+    end
+
+    assert_equal result, 0
+
+  end
+
+  def test_find_index_strings_2_block
+
+    result = @strings.find_index do |name|
+      name < 'Anna'
+    end
+
+    assert_nil result, nil
+
+  end
+
+
   # first → obj or nil
+
+  def test_first_ints
+
+    result = @integers.first
+
+    assert_equal result, 1
+
+  end
+
+  def test_first_strings
+
+    result = @strings.first
+
+    assert_equal result, "Kyle"
+
+  end
+
   # first(n) → an_array
+
+  def test_first_ints_2
+
+    result = @integers.first(2)
+
+    assert_equal result, [1, 2]
+
+  end
+
+  def test_first_strings_2
+
+    result = @strings.first(3)
+
+    assert_equal result, ["Kyle", "Preston", "Renato"]
+
+  end
+
   # flat_map { |obj| block } → array %same as collect_concat
+
+
   # grep(pattern) → array
+
+  def test_grep_ints
+
+    result = @integers.grep 1
+
+    assert_equal result, [1]
+
+  end
+
+  def test_grep_strings
+
+    result = @strings.grep 'Preston'
+
+    assert_equal result, ['Preston']
+
+  end
+
   # grep(pattern) { |obj| block} → array
+
+  def test_grep_ints_2
+
+    result = @integers.grep(1) do |num|
+      print num
+    end
+
+    print result
+
+    assert_equal result, [nil]
+
+  end
+
+  def test_grep_strings_2
+
+    result = @strings.grep('Preston')  do |name|
+      print name
+    end
+
+    assert_equal result, [nil]
+
+  end
+
   # grep_v(pattern) → array
+
+  def test_grep_v_ints
+
+    result = @integers.grep_v 1
+
+    assert_equal result, [2, 3]
+
+  end
+
+  def test_grep_v_strings
+
+    result = @strings.grep_v 'Preston'
+
+    assert_equal result, ['Kyle', 'Renato']
+
+  end
+
+
   # grep_v(pattern) { |obj| block } → array
+
+  def test_grep_v_ints_2
+
+    result = @integers.grep_v(1) do |num|
+      print num
+    end
+
+    print result
+
+    assert_equal result, [nil, nil]
+
+  end
+
+  def test_grep_v_strings_2
+
+    result = @strings.grep_v('Preston')  do |name|
+      print name
+    end
+
+    assert_equal result, [nil, nil]
+
+  end
+
+
   # group_by { |obj| block } → a_hash
+
   # include?(obj) → true or false %same as member
+
+  def test_include_int_1
+    result = @integers.include? 2
+
+    assert_equal result, true
+  end
+
+  def test_include_int_2
+    result = @integers.include? 4
+
+    assert_equal result, false
+  end
+
+  def test_include_strings_1
+    result = @strings.include? 'Kyle'
+
+    assert_equal result, true
+  end
+
+  def test_include_strings_2
+    result = @strings.include? 'Toby'
+
+    assert_equal result, false
+  end
+
   # inject(initial, sym) → obj %same as reduce (all of inject)
+
+
+
   # inject(sym) → obj
   # inject(initial) { |memo, obj| block } → obj
   # inject { |memo, obj| block } → obj
+
   # map { |obj| block } → array % same as collect
+
+  def test_map_ints
+    results = @integers.map do |num|
+      num*num
+    end
+
+    assert_equal results, [1, 4, 9]
+  end
+
+  def test_map_strings
+    results = @strings.map do |name|
+      name + '!'
+    end
+
+    assert_equal results, ['Kyle!', 'Preston!', 'Renato!']
+  end
+
+
   # max → obj
+
+  def test_max_ints
+    result = @integers.max
+
+    assert_equal result, 3
+  end
+
+  def test_max_strings
+    result = @strings.max
+
+    assert_equal result, 'Renato'
+  end
+
   # max { |a, b| block } → obj
+
+  def test_max_ints_2
+    result = @integers.max do |a, b|
+      a%2 <=> b%2
+    end
+
+    assert_equal result, 1
+  end
+
+  def test_max_strings_2
+    result = @strings.max do |a, b|
+        a.length <=> b.length
+  end
+
+    assert_equal result, 'Preston'
+  end
+
+
   # max(n) → array
+
+  def test_max_ints_n
+    result = @integers.max(2)
+
+    assert_equal result, [3,2]
+  end
+
+  def test_max_strings_n
+    result = @strings.max(2)
+
+    assert_equal result, ['Renato', 'Preston']
+  end
+
   # max(n) {|a,b| block } → obj
+
+  def test_max_ints_2_n
+    result = @integers.max(2) do |a, b|
+      (a%2) <=> (b%2)
+    end
+
+    assert_equal result, [3, 1]
+  end
+
+  def test_max_strings_2_n
+    result = @strings.max(2) do |a, b|
+      a.length <=> b.length
+    end
+
+    assert_equal result, ['Preston', 'Renato']
+  end
+
   # max_by {|obj| block } → obj
+
+  def test_max_by_ints
+    result = @integers.max_by do |num|
+      num%2
+    end
+
+    assert_equal result, 1
+  end
+
+  def test_max_by_strings
+    result = @strings.max_by do |name|
+      name.length
+    end
+
+    assert_equal result, 'Preston'
+  end
+
   # max_by(n) {|obj| block } → obj
+
+  def test_max_by_ints_n
+    result = @integers.max_by(2) do |num|
+      num%2
+    end
+
+    assert_equal result, [3,1]
+  end
+
+  def test_max_by_strings_n
+    result = @strings.max_by(2) do |name|
+      name.length
+    end
+
+    assert_equal result, ['Preston', 'Renato']
+  end
+
   # member?(obj) → true or false %same as include
+
+  def test_member_int_1
+    result = @integers.member? 2
+
+    assert_equal result, true
+  end
+
+  def test_member_int_2
+    result = @integers.member? 4
+
+    assert_equal result, false
+  end
+
+  def test_member_strings_1
+    result = @strings.member? 'Kyle'
+
+    assert_equal result, true
+  end
+
+  def test_member_strings_2
+    result = @strings.member? 'Toby'
+
+    assert_equal result, false
+  end
+
   # min → obj
   # min {| a,b | block } → obj
   # min(n) → array
